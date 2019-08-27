3611c4ba6410b84a8d0fd7d1b24d3e5947e176b7
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-27 19:46:17 +0400

    Lecture 295. Combining Multiple Reducers

---
+++ src/store/reducers/counter_reducer.js
@@ -0,0 +1,28 @@
+import * as actionTypes from "../actions";
+
+const initalState = {
+  counter: 0
+};
+
+const counter_reducer = (state = initalState, action) => {
+  let updatedState = { ...state };
+  // let updatedState = Object.assign({}, state);
+  switch (action.type) {
+    case actionTypes.INCREMENT:
+      updatedState.counter = updatedState.counter + 1;
+      break;
+    case actionTypes.DECREMENT:
+      updatedState.counter = updatedState.counter - 1;
+      break;
+    case actionTypes.ADD:
+      updatedState.counter = updatedState.counter + action.value;
+      break;
+    case actionTypes.SUBTRACT:
+      updatedState.counter = updatedState.counter - action.value;
+      break;
+    default:
+  }
+  return updatedState;
+};
+
+export default counter_reducer;
---
+++ src/store/reducers/results_reducer.js
@@ -0,0 +1,39 @@
+import * as actionTypes from "../actions";
+
+const initalState = {
+  results: [],
+  id: 0
+};
+
+const results_reducer = (state = initalState, action) => {
+  let updatedState = { ...state };
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      // push modifies the old array
+      // updatedState.results = [...updatedState.results];
+      // updatedState.results.push({
+      //   id: updatedState.id + 1,
+      //   result: updatedState.counter
+      // });
+      //
+      // Instead we can use concat which creates newArray append the value and return that newArray
+      updatedState.results = updatedState.results.concat({
+        id: updatedState.id + 1,
+        result: action.result
+      });
+      updatedState.id = updatedState.id + 1;
+      break;
+    case actionTypes.DELETE_RESULT:
+      // const oldResults = [...updatedState.results];
+      // not needed since filter already return new array instead of modyfing old one
+      const newResults = updatedState.results.filter(({ result, id }) => {
+        return id !== action.id;
+      });
+      updatedState.results = newResults;
+      break;
+    default:
+  }
+  return updatedState;
+};
+
+export default results_reducer;
