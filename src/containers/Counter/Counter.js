43546e2bcee60c1c1d2171f6da6c65302d3d83f3
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-08-27 15:52:48 +0400

    Lecture 287. Connecting the Store to React

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -36,7 +36,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <CounterOutput value={this.state.counter} />
+        <CounterOutput value={this.props.ctr} />
         <CounterControl
           label="Increment"
           clicked={() => this.counterChangeHandler("inc")}
@@ -58,4 +58,9 @@ class Counter extends Component {
   }
 }
 
-export default connect()(Counter);
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  };
+};
+export default connect(mapStateToProps)(Counter);
